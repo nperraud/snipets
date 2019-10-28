ba959afdd774b58fb67e9047766b84e77086e574
Nathanael Perraudin <nperraud@users.noreply.github.com>
2019-10-28 01:30:23 +0100

    add threaded generator

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # snipets
-Small snipets of codes to save
+Small snipets of codes. Please complain, merge request if you can do it better.
+
+* `threaded_generator.py`: transform a generator into a separate process.
---
+++ threaded_generator.py
@@ -0,0 +1,53 @@
+"""Threaded generator.
+
+    This is an example to transform a generator into a separate process.
+
+    This is useful if your generator take some time. As an example, it can be
+    ued to loads data from the disk and does some preprocessing.
+"""
+
+import multiprocessing as mp
+from time import sleep
+
+
+def get_data(l=10):
+    for i in range(l):
+        print('Get data {}'.format(i))
+        sleep(0.5)
+        yield i
+
+
+def do_func(i):
+    print('Process data {}'.format(i))
+    sleep(1)
+
+
+def queued_generator(g, maxsize=2):
+    """Transform a generator in a threaded generator."""
+    q = mp.Queue(maxsize=maxsize)
+
+    def put_data_in_queue(q, g):
+        for d in g:
+            q.put(d)
+        q.put('DONE')
+
+    reader_p = mp.Process(
+        target=put_data_in_queue, args=(
+            q,
+            g,
+        ))
+    reader_p.daemon = True
+    reader_p.start()  # Launch reader_proc() as a separate python process
+
+    while True:
+        el = q.get()
+        if el == 'DONE':
+            return
+        else:
+            yield el
+
+
+if __name__ == "__main__":
+    my_gen = queued_generator(get_data(10), maxsize=4)
+    for d in my_gen:
+        do_func(d)
