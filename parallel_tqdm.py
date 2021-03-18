3cc8876686ca50805d03d8b78b36c479a7cb10c6
nperraud <6399466+nperraud@users.noreply.github.com>
2021-03-18 11:17:12 +0100

    Update parallel_tqdm.py

--- parallel_tqdm.py
+++ parallel_tqdm.py
@@ -1,2 +1,17 @@
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
+
+from time import sleep
+from multiprocessing import cpu_count
+def do_stuff(x):
+    sleep(0.1)
+    return x + 1
+
+# r = []
+# for i in tqdm(range(100)):
+#     r.append(do_stuff(i))
+
+ncpu = cpu_count()
+
+r = process_map(do_stuff, list(range(100)), max_workers=ncpu )
+print(r)
